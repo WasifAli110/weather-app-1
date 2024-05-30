13f9c6ea2de0ad83ddf8f3bf84e5bde2a145a4be
WasifAli110 <154609808+WasifAli110@users.noreply.github.com>
2024-05-30 12:46:43 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 
+
 // https://rapidapi.com/hub
 // skeleton loading
 
@@ -11,34 +12,49 @@ const humidity = document.querySelector("#humidity");
 const message = document.querySelector("#message");
 const btn = document.querySelector("#getWeatherBtn");
 
+// swal("welcome", "you are welcome to smit additioal details", "success");
+
 const formHandler = async (event) => {
-  try {
-    event.preventDefault(); // prevents page refresh
-
-    message.innerText = "loading..."; // showing loading
-    temp.innerText = "";
-    humidity.innerText = "";
-    btn.disabled = true; // disable button so user doesn't click button more than once
-
-    const city = cityInput.value;
-
-    const response = await axios(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`,
-    );
-
-    message.innerText = ""; // clearing old msgs
-
-    form.reset(); // to clear input value only if form is submitted successfully
-
-    temp.innerText = `${response.data.main.temp}°C`;
-    humidity.innerText = response.data.main.humidity;
-
-    console.log("🚀 ~ formHandler ~ response:", response.data);
-  } catch (error) {
-    console.log(error);
-
-    message.innerText = error?.response?.data?.message || "unknown error";
-  }
-};
-
-form.addEventListener("submit", formHandler);
+    try {
+      event.preventDefault(); // prevents page refresh
+  
+      message.innerText = "loading..."; // showing loading
+      temp.innerText = "";
+      humidity.innerText = "";
+      btn.disabled = true; // disable button so user doesn't click button more than once
+  
+      const city = cityInput.value;
+  
+      const response = await axios(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`,
+      );
+  
+      form.reset(); // to clear input value only if form is submitted successfully
+  
+      temp.innerText = `${response.data.main.temp}°C`;
+      humidity.innerText = response.data.main.humidity;
+  
+      console.log("🚀 ~ formHandler ~ response:", response.data);
+  
+      //
+    } catch (error) {
+      console.log("error", error);
+  
+      swal({
+        title: "Error",
+        icon: "error",
+        text: error?.response?.data?.message || "Unknawn error please try again",
+      });
+  
+      //
+    } finally {
+      console.log("finally will run in every situation");
+  
+      message.innerText = ""; // clearing old msgs
+  
+      btn.disabled = false;
+    }
+    console.log("last line of function");
+  };
+  
+  form.addEventListener("submit", formHandler);
